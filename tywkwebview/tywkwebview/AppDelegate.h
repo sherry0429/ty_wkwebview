39742f58502616d147408d0e412484356192fad1
潘天佑 <pantianyou@63.73.65.125.broad.my.sc.dynamic.163data.com.cn>
2016-09-14 19:56:07 +0800

    first step

---
+++ tywkwebview/tywkwebview/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  tywkwebview
+//
+//  Created by 潘天佑 on 16/9/14.
+//  Copyright © 2016年 潘天佑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ tywkwebview/tywkwebview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tywkwebview
+//
+//  Created by 潘天佑 on 16/9/14.
+//  Copyright © 2016年 潘天佑. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
